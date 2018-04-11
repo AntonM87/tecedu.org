dce0c405bacea8b40e26caa8bd3971a8052a6f6a
AntonM87 <31993176+AntonM87@users.noreply.github.com>
2018-04-11 11:06:25 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,33 +3,38 @@
 
     var mainList = document.querySelector('.main-list');
     var submenu = document.querySelector('#submenu');
+    var submenus = document.querySelectorAll('#submenu');
     var targetElemForSubmenu = document.querySelector('#target-elem-for-submenu');
 
-    document.onmouseover = function (e) {
+    mainList.onmouseover = function (e) {
         var target = e.target;
         while (target.className != mainList.className) {
-            if(target.id == targetElemForSubmenu.id) {
+            if (target.id == targetElemForSubmenu.id) {
                 target.children[1].style.display = 'inline';
                 // target.children[1].style.opacity = 1;
                 // console.dir(target.children[1].style);
                 target.children[1].style.animation = 'fadeIn linear 1 150ms running';
                 setTimeout(function () {
                     target.children[1].style.opacity = 1;
-                },150 * 0.99); // %
+                }, 150 * 0.99); // %
                 break;
             }
             target = target.parentNode;
         }
     };
 
-    // document.onmouseout = function (e) {
-    //     var target = e.target;
-    //     while (target.id != targetElemForSubmenu.id || target.id != submenu.id) {
-    //         if()
-
-    //         break;
-    //     }
-    //     target = target.parentNode;
-    // };
+    mainList.onmouseout = function (e) {
+        var target = e.target;
+        while (target.className != mainList.className) {
+            if (target.id != 'target-elem-for-submenu' && target.id != 'submenu' || target.tagName != 'UL') {
+                for (var i = 0; i < submenus.length; i++) {
+                    // submenus[i].style.animation = 'fadeOut linear 1 150ms running';
+                    submenus[i].style.display = 'none';
+                }
+                break;
+            }
+            target = target.parentNode;
+        }
+    };
 
 })();
